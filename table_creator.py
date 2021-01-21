d21700858161b57c1bd92ae88b6a393d29f0e17f
Yaroslav Babych <yaroslav.babych@gmail.com>
2021-01-20 20:12:39 -0800

    Refactored

--- table_creator.py
+++ table_creator.py
@@ -7,7 +7,7 @@ class TableCreator:
     def __init__(self, equations: list):
         self.equations = equations
 
-    def get_sub_equations(self) -> set:
+    def _get_sub_equations(self) -> set:
         sub_eq = set()
         for expression in self.equations:
             sub_eq.add(expression)
@@ -20,10 +20,10 @@ class TableCreator:
                     sub_eq.add(expression[j + 1:i])
         return sub_eq
 
-    def get_variables(self) -> set:
+    def _get_variables(self) -> set:
         return set(re.findall(r"[a-zA-Z]", "".join(self.equations)))
 
-    def reformat_equations_for_eval(self) -> list:
+    def _reformat_equations_for_eval(self) -> list:
         return [
             equation.replace("∨", " or ").replace("∧", " and ")
                 .replace("t", " True ").replace("c", " False ")
@@ -32,16 +32,19 @@ class TableCreator:
         ]
 
     def create(self):
-        variables = self.get_variables()
-        table = list(itertools.product([True, False], repeat=len(variables)))
+        eq_variables = self._get_variables()
 
-        table_header = sorted(self.get_variables() | self.get_sub_equations(), key=lambda x: (len(x), x))
+        # generate every combination of True, False values for given variables
+        table = list(itertools.product([True, False], repeat=len(eq_variables)))
+
+        table_header = sorted(self._get_variables() | self._get_sub_equations(), key=lambda x: (len(x), x))
         table.insert(0, table_header)
+
         self.equations = table_header
-        expr_for_eval = self.reformat_equations_for_eval()
+        expr_for_eval = self._reformat_equations_for_eval()
 
         for i, row in enumerate(table[1:], start=1):  # skip header row
-            for j, var in enumerate(sorted(variables)):
+            for j, var in enumerate(sorted(eq_variables)):
                 globals()[var] = row[j]
             table[i] = ["T" if eval(eq) else "F" for eq in expr_for_eval]
         return table
--- uploader_to_docs.py
+++ uploader_to_docs.py
@@ -17,16 +17,15 @@ SCOPES = ['https://www.googleapis.com/auth/documents']
 DOCUMENT_ID = '1s7kXPMBMO3CwBqv26eqprqZpPk8lk-fPH3-c66D9JNI'
 
 
-def main(equations, problem_number):
+def get_creds():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # created automatically when the authorization flow completes for the first time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
 
+    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -37,45 +36,42 @@ def main(equations, problem_number):
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    return creds
 
+
+def main(equations, problem_number):
+    creds = get_creds()
     service = build('docs', 'v1', credentials=creds)
-    table = TableCreator(equations=equations).create()
+    truth_table = TableCreator(equations=equations).create()
 
-    requests = [{
+    table_creation_request = [{
         'insertText': {
-            'endOfSegmentLocation': {
-                'segmentId': ''
-            },
+            'endOfSegmentLocation': {'segmentId': ''},
             'text': f'\n#{problem_number}\n'
-        }
-    }, {
+        }}, {
         'insertTable': {
-            'rows': len(table),
-            'columns': len(table[0]),
-            'endOfSegmentLocation': {
-                'segmentId': ''
-            }
+            'rows': len(truth_table),
+            'columns': len(truth_table[0]),
+            'endOfSegmentLocation': {'segmentId': ''}
         },
     }]
 
-    service.documents().batchUpdate(documentId=DOCUMENT_ID, body={'requests': requests}).execute()
+    service.documents().batchUpdate(documentId=DOCUMENT_ID,
+                                    body={'requests': table_creation_request}).execute()
 
-    resource = {
-        "oauth2": creds,
-        "documentId": DOCUMENT_ID
-    }
+    # data needed to work with gdoctableapp
+    resource = {"oauth2": creds, "documentId": DOCUMENT_ID}
+    index_of_last_table = gdoctableapp.GetTables(resource)['tables'][-1]['index']
 
-    resource = {
-        "oauth2": creds,
-        "documentId": DOCUMENT_ID,
-        "tableIndex": gdoctableapp.GetTables(resource)['tables'][-1].get("index", 0),
-        "values": [
-            {
-                "values": table,
-                "range": {"startRowIndex": 0, "startColumnIndex": 0}
-            },
-        ]
-    }
+    resource.update({
+        "tableIndex": index_of_last_table,
+        "values": [{
+            "values": truth_table,
+            "range": {"startRowIndex": 0, "startColumnIndex": 0}
+        }]
+    })
+
+    # insert values of the truth_table into google doc
     gdoctableapp.SetValues(resource)
 
 
